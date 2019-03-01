cadc11772cc2fa2ec5acda31dcffd2adc54f6d20
Akhil Karra <2022akarra@dwight.edu>
2019-03-01 08:20:54 -0500

    Continued main.py

--- main.py
+++ main.py
@@ -16,7 +16,14 @@ op = input("Enter C to create a room or J to join a room: ")
 # Clear screen
 clear()
 
-# If creating
+# If creating...
 if op == "C":
+    # Get room name and connect
     roomname = input("Name your room: ")
     createroom(roomname, [login.usern])
+
+# If not creating...
+else:
+    # Get room name and connect
+    roomname = input("Name of the room you want to join: ")
+    joinroom(roomname, [login.usern])
